27485b949c5743cf120ff223a1ccd567a7c269c0
benlcb <barzen@campus.tu-berlin.de>
2019-03-24 02:20:21 +0100

    initial commit

---
+++ Poopiefy/app/src/main/java/com/example/poopiefy/MainActivity.java
@@ -0,0 +1,120 @@
+package com.example.poopiefy;
+
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.os.Build;
+import android.support.v4.app.NotificationCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.ScrollView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.text.DateFormat;
+import java.util.Date;
+
+import uk.co.lemberg.motiondetectionlib.MotionDetector;
+
+public class MainActivity extends AppCompatActivity {
+
+    private final String CHANNEL_ID = "ch1";
+
+    private static final String TAG = MainActivity.class.getSimpleName();
+    private MotionDetector motionDetector;
+    private ScrollView scrollLogs;
+    private TextView textLogs;
+
+    private DateFormat dateFormat;
+    private DateFormat timeFormat;
+
+    private final MotionDetector.Listener gestureListener = new MotionDetector.Listener() {
+        @Override
+        public void onGestureRecognized(MotionDetector.GestureType gestureType) {
+            //motion detected
+            showToast(gestureType.toString());
+            addLog("Gesture detected: " + gestureType);
+            Log.d(TAG, "Gesture detected: " + gestureType);
+        }
+    };
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        View decorView = getWindow().getDecorView();
+        // Hide both the navigation bar and the status bar.
+        // SYSTEM_UI_FLAG_FULLSCREEN is only available on Android 4.1 and higher, but as
+        // a general rule, you should design your app to hide the status bar whenever you
+        // hide the navigation bar.
+        int uiOptions = View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                | View.SYSTEM_UI_FLAG_FULLSCREEN;
+        decorView.setSystemUiVisibility(uiOptions);
+
+        scrollLogs = findViewById(R.id.scrollLogs);
+        textLogs = findViewById(R.id.textLogs);
+
+        //enable notifications
+        createNotificationChannel();
+
+        //Motion Detector
+        motionDetector = new MotionDetector(this, gestureListener);
+
+        try {
+            motionDetector.start();
+        }
+        catch (Exception e) {
+            e.printStackTrace();
+            showToast("Failed to start motion detector. Error:" + e);
+        }
+    }
+
+    private DateFormat getDateFormat() {
+        if (dateFormat == null) {
+            dateFormat = android.text.format.DateFormat.getDateFormat(this);
+        }
+        return dateFormat;
+    }
+
+    private DateFormat getTimeFormat() {
+        if (timeFormat == null) {
+            timeFormat = android.text.format.DateFormat.getTimeFormat(this);
+        }
+        return timeFormat;
+    }
+
+    private void addLog(String str) {
+        Date date = new Date();
+        String logStr = String.format("[%s %s] %s\n", getDateFormat().format(date), getTimeFormat().format(date), str);
+        textLogs.append(logStr);
+        scrollLogs.fullScroll(View.FOCUS_DOWN);
+    }
+
+    private void showToast(String str) {
+        Toast.makeText(MainActivity.this, str, Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    protected void onDestroy() {
+        motionDetector.stop();
+        super.onDestroy();
+    }
+
+    private void createNotificationChannel() {
+        // Create the NotificationChannel, but only on API 26+ because
+        // the NotificationChannel class is new and not in the support library
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            CharSequence name = getString(R.string.channel_name);
+            String description = getString(R.string.channel_description);
+            int importance = NotificationManager.IMPORTANCE_DEFAULT;
+            NotificationChannel channel = new NotificationChannel(CHANNEL_ID, name, importance);
+            channel.setDescription(description);
+            // Register the channel with the system; you can't change the importance
+            // or other notification behaviors after this
+            NotificationManager notificationManager = getSystemService(NotificationManager.class);
+            notificationManager.createNotificationChannel(channel);
+        }
+    }
+}
